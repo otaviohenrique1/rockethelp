4416599318b35777b25dbba9da013a8dd01e7904
otavio <otaviohelmsv@gmail.com>
2022-07-19 16:04:20 -0300

    Commit 3

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/
 import { THEME } from "./src/styles/theme";
 import { SignIn } from './src/screens/Signin';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
@@ -14,7 +15,8 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {(fontsLoaded) ? <SignIn/> : <Loading/>}
+      {/* {(fontsLoaded) ? <SignIn/> : <Loading/>} */}
+      {(fontsLoaded) ? <Home/> : <Loading/>}
     </NativeBaseProvider>
   );
 }
